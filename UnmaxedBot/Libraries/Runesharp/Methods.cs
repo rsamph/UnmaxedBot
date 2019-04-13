aafcdc83acb3711a68ef6365b6c075bce82d1c60
rsamph <amphor81@gmail.com>
2019-04-13 20:02:10 +0200

    Remove console writelines in runesharp lib

--- UnmaxedBot/Libraries/Runesharp/Methods.cs
+++ UnmaxedBot/Libraries/Runesharp/Methods.cs
@@ -17,16 +17,12 @@ namespace UnmaxedBot.Libraries.Runesharp
             request.Credentials = CredentialCache.DefaultCredentials;
             // Get the response.
             WebResponse response = request.GetResponse();
-            // Display the status.
-            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
             // Get the stream containing content returned by the server.
             Stream dataStream = response.GetResponseStream();
             // Open the stream using a StreamReader for easy access.
             StreamReader reader = new StreamReader(dataStream);
             // Read the content.
             string responseFromServer = reader.ReadToEnd();
-            // Display the content.
-            Console.WriteLine(responseFromServer);
 
             return responseFromServer;
         }
